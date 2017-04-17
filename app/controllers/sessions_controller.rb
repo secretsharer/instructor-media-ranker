72c27749b61baf9f4de1803b204c7c2cc62b5475
Olivia <ssecretsharer@gmail.com>
2017-04-17 16:30:16 -0700

    added the view path change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,45 +10,44 @@ class SessionsController < ApplicationController
       if user.nil?
         flash[:error] = "Could not login"
         redirect_to root_path
-        # else
-        #   #if it is there then save some data to session then redirect
-        #   session[:user_id] = user.id
-        #   flash[:success] = "Logged in Successfully"
-        #   redirect_to root_path
       end
     end
+    #   #if it is there then save some data to session then redirect
+    session[:user_id] = user.id
+    flash[:success] = "Logged in Successfully"
+    redirect_to root_path
+  end
 
-    def login_form
-    end
+  def login_form
+  end
 
-    def login
-      username = params[:username]
-      if username and user = User.find_by(username: username)
+  def login
+    username = params[:username]
+    if username and user = User.find_by(username: username)
+      session[:user_id] = user.id
+      flash[:status] = :success
+      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+    else
+      user = User.new(username: username)
+      if user.save
         session[:user_id] = user.id
         flash[:status] = :success
-        flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
       else
-        user = User.new(username: username)
-        if user.save
-          session[:user_id] = user.id
-          flash[:status] = :success
-          flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-        else
-          flash.now[:status] = :failure
-          flash.now[:result_text] = "Could not log in"
-          flash.now[:messages] = user.errors.messages
-          render "login_form", status: :bad_request
-          return
-        end
+        flash.now[:status] = :failure
+        flash.now[:result_text] = "Could not log in"
+        flash.now[:messages] = user.errors.messages
+        render "login_form", status: :bad_request
+        return
       end
-      redirect_to root_path
     end
+    redirect_to root_path
+  end
 
-    def logout
-      session[:user_id] = nil
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged out"
-      redirect_to root_path
-    end
+  def logout
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
   end
 end
